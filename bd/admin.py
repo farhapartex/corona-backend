83373a8918766efab9d707c7a3cf22b0e7e5790e
Md Nazmul Hasan <farhapartex@gmail.com>
2020-05-10 02:39:31 +0600

    BDInfo model created

--- bd/admin.py
+++ bd/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from bd.models import *
 # Register your models here.
+
+@admin.register(BDInfo)
+class BDInfoAdmin(admin.ModelAdmin):
+    list_display = ("id", "patient", "died", "recovered", "created_at")
--- bd/models.py
+++ bd/models.py
@@ -5,4 +5,16 @@ from django.conf import settings
 from system.models import Base
 # Create your models here.
 
-# class BDInfo(models.Model):
+class BDInfo(Base):
+    patient = models.IntegerField(_("New Patient today"))
+    total_patient = models.IntegerField(_("Total Patient"))
+    died = models.IntegerField(_("Died today"))
+    total_died = models.IntegerField(_("Total Died"))
+    recovered = models.IntegerField(_("Recovered Today"))
+    total_recovered = models.IntegerField(_("Total Recovered"))
+    tested = models.IntegerField(_("Tested Today"))
+    total_tested = models.IntegerField(_("Total Tested"))
+
+    def __str__(self):
+        return self.created_at
+
