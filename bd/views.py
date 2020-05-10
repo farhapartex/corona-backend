9c5f10cf7c2ce743a3efa39be401ca9260ca4ca7
Md Nazmul Hasan <farhapartex@gmail.com>
2020-05-11 05:12:24 +0600

    latest

--- bd/views.py
+++ bd/views.py
@@ -4,8 +4,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
+from datetime import datetime
 # Create your views here.
 
 class BDInfoAPIView(viewsets.ModelViewSet):
     queryset = BDInfo.objects.all()
     serializer_class = BDInfoSerializer
+
+    def get_queryset(self):
+        today = datetime.today().date()
+        queryset = BDInfo.objects.filter(created_at__date=today)
+        return queryset
